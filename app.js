0401e6f7f85d1ba02912b51c7028cd62336bd3e1
Raxa Hansana <imraxa@outlook.com>
2021-06-15 12:40:28 -0700

    added ball to move indefinantly, adjusted collision of ball/paddle to change direction of travel

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext('2d');
+let speed = 5;
 
 const player1 = { x: 50, y: 50, speed:5, width:15,height:100};
 const keyz1 = { ArrowRight: false, ArrowLeft: false, ArrowUp: false, ArrowDown: false };
 
 const player2 = { x: 550, y: 50, speed:5, width:15, height:100};
 const keyz2 = { KeyD: false, KeyA: false, KeyW: false, KeyS: false };
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    width: 10,
+    height: 10,
+    xs: speed,
+    ys: -speed
+}
 
 requestAnimationFrame(draw);
 
@@ -40,6 +50,37 @@ function move() {
 
     if (keyz2.KeyD) {player2.x += player2.speed} else if (keyz2.KeyA) {player2.x -= player2.speed};
     if (keyz2.KeyW) { player2.y -= player2.speed } else if (keyz2.KeyS) { player2.y += player2.speed };
+
+    ball.x += ball.xs;
+    ball.y += ball.ys;
+    if ((ball.x < 0 || ball.x > canvas.width)) {
+        ball.xs *= -1
+    }
+    if ((ball.y < 0 || ball.y > canvas.height)) {
+        ball.ys *= -1
+    }
+
+    if (checkCol(ball, player1)) {
+        ball.xs *= -1;
+        let temp = ((player1.y + player1.height) / 2);
+        let temp1 = ((ball.y + ball.height) / 2);
+        if (temp < temp1) {
+            ball.ys = speed
+        } else {
+            ball.ys = -speed
+        }
+        }
+    
+    if (checkCol(ball,player2)) {
+        ball.xs *= -1;
+        let temp = ((player2.y + player2.height) / 2);
+        let temp1 = ((ball.y + ball.height) / 2);
+        if (temp < temp1) {
+            ball.ys = speed
+        } else {
+            ball.ys = -speed
+        }
+    }
 }
 
 function checkCol(ob1, ob2) {
@@ -81,6 +122,10 @@ function draw() {
     ctx.fillStyle = 'red';
     ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
     
+    // ball
+    ctx.fillStyle = 'white';
+    ctx.fillRect(ball.x,ball.y,ball.width,ball.height)
+
     // track output
     let output = `Player 1 X ${player1.x} Y ${player1.y} Player 2 X ${player2.x} Y ${player2.y}`;
 
