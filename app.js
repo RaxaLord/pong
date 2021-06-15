9b28f2ae4936940dce634efacf9511e3a10970d7
Raxa Hansana <imraxa@outlook.com>
2021-06-15 11:35:46 -0700

    added second player, additional keys for second player, eleminating use of other buttons

--- app.js
+++ app.js
@@ -1,54 +1,73 @@
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext('2d');
-const player = { x: 50, y: 50, speed:5};
-const keyz = { ArrowRight: false, ArrowLeft: false, ArrowUp: false, ArrowDown: false };
+
+const player1 = { x: 50, y: 50, speed:5, width:15,height:100};
+const keyz1 = { ArrowRight: false, ArrowLeft: false, ArrowUp: false, ArrowDown: false };
+
+const player2 = { x: 550, y: 50, speed:5, width:15, height:100};
+const keyz2 = { KeyD: false, KeyA: false, KeyW: false, KeyS: false };
+
 draw();
 
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
 
+// fucntion to follow key press
 function keyDown(e) {
-    keyz[e.code] = true;
-    console.log(keyz)
+    if (e.code in keyz1) {
+        keyz1[e.code] = true;
+    }
+    if (e.code in keyz2) {
+        keyz2[e.code] = true;
+    }
+    // console.log(e.code)
     move();
 }
 
+// function to follow key release
 function keyUp(e) {
-    keyz[e.code] = false;
-    console.log(keyz)
+    if (e.code in keyz1) {
+        keyz1[e.code] = false;
+    }
+    if (e.code in keyz2) {
+        keyz2[e.code] = false;
+    }
+    // console.log(keyz2)
 }
 
+// moving function
+// this function allows the player to move, it takes each keystroke and increases or decreases postion
 function move() {
-    if (keyz.ArrowRight) {player.x+=player.speed} else if (keyz.ArrowLeft) {player.x-=player.speed};
-    if (keyz.ArrowUp) { player.y -= player.speed } else if (keyz.ArrowDown) { player.y += player.speed };
-    draw();
+    if (keyz1.ArrowRight) {player1.x += player1.speed} else if (keyz1.ArrowLeft) {player1.x -= player1.speed};
+    if (keyz1.ArrowUp) { player1.y -= player1.speed } else if (keyz1.ArrowDown) { player1.y += player1.speed };
 
+    if (keyz2.KeyD) {player2.x += player2.speed} else if (keyz2.KeyA) {player2.x -= player2.speed};
+    if (keyz2.KeyW) { player2.y -= player2.speed } else if (keyz2.KeyS) { player2.y += player2.speed };
+
+    draw();
 }
 
+// drawing function
+// this function creates the 'animation' of a moving object, takes the input of the keystrokes and generates the styling while clearing its last position
 function draw() {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    let output = `Pos X ${player.x} y ${player.y}`
+    // clear history of drawing
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#ffffff'
-    ctx.fillRect(player.x, player.y, 100, 100);
-    ctx.font = '24px serif';
+
+    // player 1
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+    // player 2
+    ctx.fillStyle = 'red';
+    ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+    
+    // track output
+    let output = `Player 1 X ${player1.x} Y ${player1.y} Player 2 X ${player2.x} Y ${player2.y}`;
+
+    // text content
+    ctx.font = '12px serif';
     ctx.textAlign = 'center'
     ctx.fillStyle = 'red';
     ctx.fillText(output, 300, 30);
-
-    //triangle
-    // ctx.beginPath();
-    // ctx.fillStyle = 'blue'
-    // ctx.moveTo(50, 200);
-    // ctx.lineTo(150, 250);
-    // ctx.lineTo(150, 150);
-    // ctx.fill();
-
-    //circle
-    // ctx.beginPath();
-    // ctx.fillStyle = 'green'
-    // ctx.arc(150, 300, 50, 0, 2 * Math.PI);
-    // ctx.strokeStyle = 'yellow'
-    // ctx.fill();
-    // ctx.stroke();
 }
