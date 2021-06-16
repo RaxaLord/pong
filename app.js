95effca1b6d5734d863e9bd12a178fb676687c2f
Raxa Hansana <imraxa@outlook.com>
2021-06-16 11:05:02 -0700

    finished game, all working.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext('2d');
 let speed = 5;
-
-const player1 = { x: 50, y: 50, speed:5, width:15,height:100};
+const player1 = { x: 50, y: 50, speed:5, width:35, height:100, score:0};
 const keyz1 = { ArrowRight: false, ArrowLeft: false, ArrowUp: false, ArrowDown: false };
-
-const player2 = { x: 550, y: 50, speed:5, width:15, height:100};
+const player2 = { x: 550, y: 50, speed:5, width:35, height:100, score:0};
 const keyz2 = { KeyD: false, KeyA: false, KeyW: false, KeyS: false };
-
 const ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -16,9 +13,23 @@ const ball = {
     xs: speed,
     ys: -speed
 }
+const div = document.createElement('div')
+const btn = document.createElement('button');
+btn.textContent = 'Game Reset'
+btn.addEventListener('click', () => {
+    //reset game
+    player1.score = 0;
+    player2.score = 0;
+    ballReset();
+    player1.x = 50;
+    player2.x = canvas.width - (50 + player2.width);
+    player1.y = canvas.height/2 - player1.height/ 2;
+    player2.y = canvas.height/2 - player2.height/ 2;
+})
+document.body.prepend(div)
+div.append(btn)
 
 requestAnimationFrame(draw);
-
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
 
@@ -31,7 +42,6 @@ function keyDown(e) {
         keyz2[e.code] = true;
     }
 }
-
 // function to follow key release
 function keyUp(e) {
     if (e.code in keyz1) {
@@ -41,18 +51,30 @@ function keyUp(e) {
         keyz2[e.code] = false;
     }
 }
-
 // moving function
 // this function allows the player to move, it takes each keystroke and increases or decreases postion
 function move() {
-    if (keyz1.ArrowRight) {player1.x += player1.speed} else if (keyz1.ArrowLeft) {player1.x -= player1.speed};
+    if (keyz1.ArrowRight && player1.x < (canvas.width/2-player1.width)) {player1.x += player1.speed} else if (keyz1.ArrowLeft && player1.x > 0) {player1.x -= player1.speed};
     if (keyz1.ArrowUp) { player1.y -= player1.speed } else if (keyz1.ArrowDown) { player1.y += player1.speed };
 
-    if (keyz2.KeyD) {player2.x += player2.speed} else if (keyz2.KeyA) {player2.x -= player2.speed};
+    if (keyz2.KeyD && player2.x < (canvas.width-player2.width)) {player2.x += player2.speed} else if (keyz2.KeyA && player2.x > (canvas.width/2-player2.width)) {player2.x -= player2.speed};
     if (keyz2.KeyW) { player2.y -= player2.speed } else if (keyz2.KeyS) { player2.y += player2.speed };
 
     ball.x += ball.xs;
     ball.y += ball.ys;
+
+    // update score if ball hits on side of wall
+    if (ball.x < 0) {
+        player2.score++;
+        ballReset()
+    }
+
+    if (ball.x > canvas.width) {
+        player1.score++;
+        ballReset()
+    }
+
+
     if ((ball.x < 0 || ball.x > canvas.width)) {
         ball.xs *= -1
     }
@@ -82,11 +104,8 @@ function move() {
         }
     }
 }
-
+// check collision
 function checkCol(ob1, ob2) {
-    // console.log(ob1.x);
-    // console.log(ob2.x+ob2.width);
-
 /*     if(ob1.x<ob2.x+ob2.width && ob1.x+ob1.width > ob2.x)
     {
         console.log('X hit')
@@ -96,14 +115,18 @@ function checkCol(ob1, ob2) {
     {
         console.log('Y hit')
     } */
-
     let val = ob1.x<ob2.x+ob2.width && ob1.x+ob1.width > ob2.x && ob1.y<ob2.y+ob2.height && ob1.y+ob1.height > ob2.y
-
     if (val) {
         console.log(val);
     }
     return val;
 }
+function ballReset() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.xs = speed;
+    ball.ys = -speed
+    }
 
 // drawing function
 // this function creates the 'animation' of a moving object, takes the input of the keystrokes and generates the styling while clearing its last position
@@ -126,13 +149,14 @@ function draw() {
     ctx.fillStyle = 'white';
     ctx.fillRect(ball.x,ball.y,ball.width,ball.height)
 
-    // track output
-    let output = `Player 1 X ${player1.x} Y ${player1.y} Player 2 X ${player2.x} Y ${player2.y}`;
+    // track score
+    // let output = `Player 1 X ${player1.x} Y ${player1.y} Player 2 X ${player2.x} Y ${player2.y}`;
+    let output = `Player1 : ${player1.score} vs Player2 : ${player2.score}`
 
     // text content
-    ctx.font = '12px serif';
+    ctx.font = '18px serif';
     ctx.textAlign = 'center'
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'white';
     ctx.fillText(output, 300, 30);
 
     requestAnimationFrame(draw);
